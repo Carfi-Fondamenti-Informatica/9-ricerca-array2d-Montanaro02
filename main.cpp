7e2a04d3508331b8226f2ab2fa3127a8ee1cdcb4
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-15 19:44:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,43 +3,6 @@
 #include "lib.h"
 using namespace std;
 
-void init(char mat[10][20]){
-    for(int i=0;i<10;i++){
-        for(int j=0;j<20;j++){
-            mat[i][j] = ' ';
-        }
-    }
-}
-
-void init(char ar[20]){
-    for(int i=0; i<20;i++){
-        ar[i] = ' ';
-    }
-}
-
-void init (char [10][20]);
-void init (char [20]);
-int ricerca (char [10][20], char [20]);
-
-int ricerca(char nomi[10][20], char seleziona[20]){
-    for(int i=0; i<10; i++){
-        int a=0;
-        for(int j=0;j<20; j++){
-            if(nomi[i][j]==seleziona[j]){
-                a++;
-            }
-            else {
-                break;
-            }
-        }
-        if(a==20){
-            return i;
-        }
-    }
-    return -1;
-}
-
-
 int main(){
     char lista[10][20];
     char cerca[20];
